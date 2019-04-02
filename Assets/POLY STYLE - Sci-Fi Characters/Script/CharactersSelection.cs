131d316d3b109a253e42e7ff3cc3e22df5ee2a70
Memo Khoury <majd.khoury93@gmail.com>
2019-04-01 20:29:09 -0400

    assets: imported assets

---
+++ Assets/POLY STYLE - Sci-Fi Characters/Script/CharactersSelection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersSelection : MonoBehaviour {
+
+	public GameObject character1;
+	public GameObject character2;
+	public GameObject character3;
+	public GameObject character4;
+	public GameObject character5;
+	public GameObject character6;
+
+
+	void setCharacter01 (){
+		onActiveFalse ();
+		character1.active=true;
+	}
+	void setCharacter02 (){
+		onActiveFalse ();
+		character2.active=true;
+	}
+	void setCharacter03 (){
+		onActiveFalse ();
+		character3.active=true;
+	}
+	void setCharacter04 (){
+		onActiveFalse ();
+		character4.active=true;
+	}
+	void setCharacter05 (){
+		onActiveFalse ();
+		character5.active=true;
+	}
+	void setCharacter06 (){
+		onActiveFalse ();
+		character6.active=true;
+	}
+
+	void onActiveFalse()
+	{
+		character1.active=false;
+		character2.active=false;
+		character3.active=false;
+		character4.active=false;
+		character5.active=false;
+		character6.active=false;
+
+	}
+}
---
+++ Assets/POLY STYLE - Sci-Fi Characters/Script/ObjectRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotate : MonoBehaviour {
+	public float a;
+
+	void Play () {
+		a = 0.3F;
+	}
+
+	void Stop () {
+		a = 0;
+	}
+
+	void Update () {
+		transform.Rotate(0, a, 0);
+	}
+}
